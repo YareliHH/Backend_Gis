d9337e0853b4cd0dbfae8c8783dfe120610c1b78
Yareli HH <yhh122004@gmail.com>
2025-07-21 13:34:28 -0600

    adddd

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.use('/api', banner);
 app.use('/api', chat);
 app.use('/api', promociones);
 app.use('/api', mercadopago);
-app.use('/api', direcciones);
+app.use('/api/direcciones', direcciones);
 
 
 // Iniciar el servidor
